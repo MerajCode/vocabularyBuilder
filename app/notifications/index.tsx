d80666ec14f20b1d664feb0819c7859a34380696
Meraj Alam <merajansari81034@gmail.com>
2025-05-26 17:18:21 +0530

    Change expo notification to native notification by forground service

--- app/notifications/index.tsx
+++ app/notifications/index.tsx
@@ -1,8 +1,6 @@
-import { initializeNotifications } from "@/components/notify";
 import { ThemedText } from "@/components/ThemedText";
 import WordsDB from "@/controller/handler";
 import { useColorScheme } from "@/hooks/useColorScheme.web";
-import * as Notifications from "expo-notifications";
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
 import {
@@ -10,74 +8,66 @@ import {
   Button,
   ScrollView,
   StyleSheet,
-  Switch,
   TextInput,
   View,
 } from "react-native";
 
-initializeNotifications();
-
 export default function Index() {
   const [customTime, setCustomTime] = useState("10"); // in seconds
   const [type, setType] = useState("verb"); // in seconds
-  const [repeat, setRepeat] = useState(false);
-
-  //   const { WordNotificationModule } = NativeModules;
-
-  // type WordData = {
-  //   word: string;
-  //   type?: string | null;
-  //   meaning?: string | null;
-  //   form1?: string | null;
-  //   form2?: string | null;
-  //   form3?: string | null;
-  //   example?: string | null;
-  // };
-
-  // const showNativeWordNotification = (word: WordData) => {
-  //   WordNotificationModule.showNotification(word);
-  // };
 
   const navigation = useRouter();
 
-  const scheduleNotificationAsync = async (
-    seconds?: number,
-    repeats = false
-  ) => {
-    const word = await WordsDB.getRandWord(type); // 👈 Use your existing function
-    if (!word) {
+  const insertNotification = async () => {
+    const result = await WordsDB.insertNotification({
+      repeat_interval: Number(customTime),
+      type,
+    });
+    if (!result) {
       Alert.alert("Error", "Somthing Went Wrong!");
-    } else {
-      // 💬 Create dynamic message based on word type
-      let title = `${word.word}: ${word.meaning}`;
-      let body = `Example : ${word.example}`;
-      if (word.type === "verb") {
-        body = `1st: ${word.form1} | 2nd: ${word.form2} | 3rd: ${word.form3}`;
-      }
-      await Notifications.scheduleNotificationAsync({
-        content: {
-          title: title,
-          body: body,
-          priority: Notifications.AndroidNotificationPriority.HIGH,
-          data: { type },
-        },
-        trigger: seconds
-          ? {
-              type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
-              seconds,
-              repeats,
-            }
-          : null,
-      });
-      Alert.alert("Success", "Scheduled successfully.");
+      return;
     }
+    Alert.alert("Success", "ID : " + result + " Scheduled successfully.");
   };
+  // const scheduleNotificationAsync = async (
+  //   seconds?: number,
+  //   repeats = false
+  // ) => {
+  //   const word = await WordsDB.getRandWord(type);
+  //   if (!word) {
+  //     Alert.alert("Error", "Somthing Went Wrong!");
+  //   } else {
+  //     // 💬 Create dynamic message based on word type
+  //     let title = `${word.word}: ${word.meaning}`;
+  //     let body = `Example : ${word.example}`;
+  //     if (word.type === "verb") {
+  //       body = `1st: ${word.form1} | 2nd: ${word.form2} | 3rd: ${word.form3}`;
+  //     }
+  //     await Notifications.scheduleNotificationAsync({
+  //       content: {
+  //         title: title,
+  //         body: body,
+  //         priority: Notifications.AndroidNotificationPriority.HIGH,
+  //         data: { type },
+  //       },
+  //       trigger: seconds
+  //         ? {
+  //             type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+  //             seconds,
+  //             repeats,
+  //           }
+  //         : null,
+  //     });
+  //     Alert.alert("Success", "Scheduled successfully.");
+  //   }
+  // };
 
   const isDarkMode = useColorScheme();
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {/* Time Input */}
+      <ThemedText style={styles.header}>Schedule Notification</ThemedText>
       <View style={{ paddingVertical: 10 }}>
         <ThemedText>Custom Time (in minuts)</ThemedText>
         <TextInput
@@ -98,24 +88,16 @@ export default function Index() {
         />
       </View>
 
-      {/* Repeat Switch */}
-      <View style={styles.row}>
-        <ThemedText>Repeat Notification</ThemedText>
-        <Switch value={repeat} onValueChange={setRepeat} />
-      </View>
-
       {/* Buttons */}
       <View style={styles.section}>
-        <Button
-          title="Schedule Notification"
-          onPress={() =>
-            scheduleNotificationAsync(Number(customTime) * 60, repeat)
-          }
-        />
+        <Button title="Schedule Notification" onPress={insertNotification} />
         <Button
           title="Schedule List"
           onPress={() => navigation.push("/notifications/scheduled")}
         />
+      </View>
+      <View style={styles.section}>
+        <ThemedText style={{ textAlign: "center" ,fontSize:13,fontStyle:"italic"}}>Minimum allowed time is 1 minute. Please set your custom timer.</ThemedText>
       </View>
     </ScrollView>
   );
@@ -130,6 +112,7 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "bold",
     marginBottom: 20,
+    marginTop: 40,
     textAlign: "center",
   },
   row: {
