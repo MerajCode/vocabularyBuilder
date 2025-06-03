b82dd5c6738a2b7b36bc6ac27cbaa438d1db39d2
Meraj Alam <merajansari81034@gmail.com>
2025-06-03 20:15:16 +0530

    Modify: convert native usage to Kotlin and reorganize code structure

--- android/app/src/main/java/com/merajalam/VocabBuilder/ScreenEvent/ScreenReceiver.kt
+++ android/app/src/main/java/com/merajalam/VocabBuilder/ScreenEvent/ScreenReceiver.kt
@@ -5,38 +5,23 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 
-class ScreenReceiver : BroadcastReceiver() {
+interface ScreenEventListener {
+    fun onScreenOn()
+    fun onUserPresent()
+}
+
+class ScreenReceiver() : BroadcastReceiver() {
+    var listener: ScreenEventListener? = null
     override fun onReceive(context: Context?, intent: Intent?) {
-        val action = intent?.action
-        Log.d("ScreenReceiver", "Received action: $action")
-
-        when (action) {
-            Intent.ACTION_SCREEN_OFF -> {
-                Log.d("ScreenReceiver", "📴 Screen OFF")
-                if (NativeEventModule.getInstance() != null) {
-                    NativeEventModule.getInstance()?.sendEvent("SCREEN_OFF")
-                } else {
-                    Log.w("ScreenReceiver", "⚠️ NativeEventModule not initialized — screen event skipped")
-                }
-
-            }
+        when (intent?.action) {
             Intent.ACTION_SCREEN_ON -> {
                 Log.d("ScreenReceiver", "🔓 Screen ON")
-
-                if (NativeEventModule.getInstance() != null) {
-                    NativeEventModule.getInstance()?.sendEvent("SCREEN_ON")
-                } else {
-                    Log.w("ScreenReceiver", "⚠️ NativeEventModule not initialized — screen event skipped")
-                }
+                listener?.onScreenOn()
             }
+
             Intent.ACTION_USER_PRESENT -> {
                 Log.d("ScreenReceiver", "👤 User Present (unlocked)")
-
-                if (NativeEventModule.getInstance() != null) {
-                    NativeEventModule.getInstance()?.sendEvent("USER_PRESENT")
-                } else {
-                    Log.w("ScreenReceiver", "⚠️ NativeEventModule not initialized — screen event skipped")
-                }
+                listener?.onUserPresent()
             }
         }
     }
