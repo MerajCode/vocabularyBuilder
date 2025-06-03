6cb537aaedb4344b3e02c8e4eb825ca4d3527064
Meraj Alam <merajansari81034@gmail.com>
2025-06-03 20:03:27 +0530

    Add : dropdown word type data

---
+++ utils/staticData.ts
@@ -0,0 +1,24 @@
+const opts = [
+  { label: "Noun", value: "noun" },
+  { label: "Pronoun", value: "pronoun" },
+  { label: "Verb", value: "verb" },
+  { label: "Adverb", value: "adverb" },
+  { label: "Adjective", value: "adjective" },
+  { label: "Preposition", value: "preposition" },
+  { label: "Conjunction", value: "conjunction" },
+  { label: "Interjection", value: "interjection" },
+  { label: "Article", value: "article" },
+  { label: "Determiner", value: "determiner" },
+  { label: "Auxiliary Verb", value: "auxiliary-verb" },
+  { label: "Modal Verb", value: "modal-verb" },
+  { label: "Gerund", value: "gerund" },
+  { label: "Participle", value: "participle" },
+  { label: "Infinitive", value: "infinitive" },
+  { label: "Phrase", value: "phrase" },
+  { label: "Idiom", value: "idiom" },
+  { label: "Abbreviation", value: "abbreviation" },
+  { label: "Prefix", value: "prefix" },
+  { label: "Suffix", value: "suffix" },
+];
+
+export { opts };
