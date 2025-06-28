a3095e5baa9e1d6d8a9f83b9c7f5f42c127f3b75
Meraj Alam <merajansari81034@gmail.com>
2025-06-28 11:12:46 +0530

    Modify: change manual migration to auto

--- controller/database.ts
+++ controller/database.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
+import { migrate } from "drizzle-orm/expo-sqlite/migrator";
 import * as SQLite from "expo-sqlite";
+import migrations from '../drizzle/migrations';
 
 export const databaseName = "myvb.db";
 
@@ -7,41 +9,7 @@ export const sqliteDb = SQLite.openDatabaseSync(databaseName, {
   enableChangeListener: true,
 });
 
+// Run migrations
+migrate(drizzle(sqliteDb), migrations);
+
 export const db = drizzle(sqliteDb);
-
-sqliteDb.execSync(`
-  CREATE TABLE IF NOT EXISTS words (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    word TEXT NOT NULL,
-    type TEXT,
-    meaning TEXT,
-    form1 TEXT,
-    form2 TEXT,
-    form3 TEXT,
-    example TEXT,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP
-  );
-`);
-
-sqliteDb.execSync(`
-  CREATE TABLE IF NOT EXISTS settings (
-	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	type text,
-	timer integer NOT NULL,
-	unlock_type text,
-	unlock_status integer DEFAULT false,
-	present_type text,
-	present_status integer DEFAULT false,
-	created_at text DEFAULT CURRENT_TIMESTAMP
-);
-`);
-
-sqliteDb.execSync(`
-  CREATE TABLE IF NOT EXISTS scheduled_notifications (
-	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	type text,
-	timer integer NOT NULL,
-	is_active integer DEFAULT 1 NOT NULL,
-	created_at text DEFAULT CURRENT_TIMESTAMP
-);
-`);
