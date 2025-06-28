84f2744d8f60f21e4cabc486020506e9d63a8e5f
Meraj Alam <merajansari81034@gmail.com>
2025-06-28 11:09:29 +0530

    Migration for apiKey

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -2,11 +2,13 @@
 
 import journal from './meta/_journal.json';
 import m0000 from './0000_even_apocalypse.sql';
+import m0001 from './0001_curious_christian_walker.sql';
 
   export default {
     journal,
     migrations: {
-      m0000
+      m0000,
+m0001
     }
   }
   
